fbc4f0be3ac2d9a72cee3c43a4aa91d0893d90aa
nozomail <nozomi.mail@icloud.com>
2020-11-19 22:53:53 +1300

    Add BlogPost template and setup createPage

---
+++ src/templates/blog-post.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { graphql, Link } from 'gatsby';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+
+import Layout from '../components/layout';
+
+export const query = graphql`
+  query($slug: String!) {
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      publishedDate(formatString: "Do MMMM, YYYY")
+      body {
+        raw
+      }
+    }
+  }
+`;
+
+type dataProps = {
+  data: {
+    contentfulBlogPost: {
+      title: string;
+      publishedDate: string;
+      body: {
+        raw: string;
+      };
+    };
+  };
+};
+
+const BlogPost: React.FC<dataProps> = ({ data }) => {
+  return (
+    <Layout>
+      <h1>Blog page</h1>
+      <Link to="/blog/">Visit the Blog Page</Link>
+      <div className="content">
+        <h1>{data.contentfulBlogPost.title}</h1>
+        <div className="meta">
+          Posted on {data.contentfulBlogPost.publishedDate}
+        </div>
+        {documentToReactComponents(
+          JSON.parse(data.contentfulBlogPost.body.raw)
+        )}
+      </div>
+    </Layout>
+  );
+};
+
+export default BlogPost;
