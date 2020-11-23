2267b5d5d725ef16638ba1af7c2ae9975b3a3964
nozomail <nozomi.mail@icloud.com>
2020-11-23 23:55:26 +1300

    Change header/layout design

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,12 +5,17 @@ const Header: React.FC = () => {
   return (
     <header className="flex justify-between py-4">
       <div>
-        <Link to="/">Logo</Link>
+        <Link to="/" className="flex items-center">
+          <div className="w-8 h-8 rounded-full bg-pink-200"></div>
+          <div className="text-2xl text-blue-600 font-semibold ml-2">
+            Biofab.blog
+          </div>
+        </Link>
       </div>
       <nav>
-        <ul className="flex">
+        <ul className="flex text-gray-300">
           <li>
-            <Link to="/blog/">Blog</Link>
+            <Link to="/articles/">Articles</Link>
           </li>
           <li className="ml-8">
             <Link to="/about/">About</Link>
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,9 +9,20 @@ type Props = {
 const Layout: React.FC<Props> = ({ title, children }) => {
   return (
     <div>
-      <div className="bg-indigo-400 text-white pt-2 pb-8 px-8">
+      <div className="bg-gray-100 pt-2 pb-8 px-8">
         <Header />
-        <h1 className="text-5xl mt-6">{title}</h1>
+        {title === 'Home' ? (
+          <div className="text-5xl font-semibold py-10">
+            <span className="block py-2 text-blue-400">
+              Unbiased report on biofabrication
+            </span>
+            <span className="block py-2 text-lightBlue-300">
+              for beginners to advanced
+            </span>
+          </div>
+        ) : (
+          <h1 className="text-5xl text-blue-400 font-semibold mt-6">{title}</h1>
+        )}
       </div>
       <div className="py-8 px-8">{children}</div>
     </div>
