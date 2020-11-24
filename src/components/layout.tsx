807b56b2b4f1ac7c3ed670b9189b0df80320e137
nozomail <nozomi.mail@icloud.com>
2020-11-24 23:44:41 +1300

    Add latest articles/where to start sections to home page

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,22 +9,15 @@ type Props = {
 const Layout: React.FC<Props> = ({ title, children }) => {
   return (
     <div>
-      <div className="bg-gray-100 pt-2 pb-8 px-8">
-        <Header />
-        {title === 'Home' ? (
-          <div className="text-5xl font-semibold py-10">
-            <span className="block py-2 text-blue-400">
-              Unbiased report on biofabrication
-            </span>
-            <span className="block py-2 text-lightBlue-300">
-              for beginners to advanced
-            </span>
-          </div>
-        ) : (
-          <h1 className="text-5xl text-blue-400 font-semibold mt-6">{title}</h1>
-        )}
+      <div className="bg-blue-200 pt-2 pb-8 px-8">
+        <Header dark />
+        <h1 className="max-w-screen-lg mx-auto text-5xl font-serif text-blue-400 mt-6">
+          {title}
+        </h1>
       </div>
-      <div className="py-8 px-8">{children}</div>
+      <div className="py-8 px-8">
+        <div className="max-w-screen-lg mx-auto">{children}</div>
+      </div>
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,37 @@
 import React from 'react';
 import { useStaticQuery, graphql, Link } from 'gatsby';
+import Img, { FluidObject } from 'gatsby-image';
+import BackgroundImg, { IFluidObject } from 'gatsby-background-image';
 
-import Layout from '../components/layout';
+import Header from '../components/header';
 
-type edgeProps = {
+type articleProps = {
   node: {
     id: string;
     title: string;
     slug: string;
     publishedDate: string;
+    mainImage: {
+      fluid: FluidObject;
+    };
+  };
+};
+
+type categoryProps = {
+  node: {
+    id: string;
+    name: string;
+    slug: string;
+    image: {
+      fluid: IFluidObject;
+    };
   };
 };
 
 const Index: React.FC = () => {
-  const data = useStaticQuery(graphql`
+  const { articles, categories } = useStaticQuery(graphql`
     query {
-      allContentfulBlogPost(
+      articles: allContentfulBlogPost(
         limit: 3
         sort: { order: DESC, fields: publishedDate }
       ) {
@@ -25,45 +41,106 @@ const Index: React.FC = () => {
             id
             title
             publishedDate(formatString: "DD MMM, YYYY")
+            mainImage {
+              fluid(maxWidth: 160) {
+                ...GatsbyContentfulFluid
+              }
+            }
+          }
+        }
+      }
+      categories: allContentfulBlogCategory(
+        sort: { order: ASC, fields: order }
+      ) {
+        edges {
+          node {
+            name
+            slug
+            id
+            image {
+              fluid(maxWidth: 750) {
+                ...GatsbyContentfulFluid
+              }
+            }
           }
         }
       }
     }
   `);
 
+  console.log('articles', articles);
+  console.log('categories', categories);
+
   return (
-    <Layout title="Home">
-      <ul>
-        {data.allContentfulBlogPost.edges.map((edge: edgeProps) => {
-          return (
-            <li key={edge.node.id} className="border-b border-gray-200">
-              <Link
-                to={`/blog/${edge.node.slug}/`}
-                className="flex items-start p-6"
+    <div>
+      <div className="bg-gray-100 pt-2 pb-8 px-8">
+        <Header />
+        <div className="max-w-screen-lg mx-auto font-serif py-10">
+          <span className="block py-2 text-5xl leading-tight text-blue-400">
+            Unbiased Report
+            <br />
+            on <span className="text-pink-300">Biofabrication</span>
+          </span>
+          <span className="block py-2 text-3xl italic text-blue-300">
+            for beginners to advanced
+          </span>
+        </div>
+      </div>
+
+      <section className="py-20 px-8">
+        <h2 className="text-lg font-semibold text-green-400 tracking-wider text-center mb-6">
+          LATEST ARTICLES
+        </h2>
+        <ul className="max-w-screen-md mx-auto">
+          {articles.edges.map((edge: articleProps) => {
+            return (
+              <li
+                key={edge.node.id}
+                className="border-b border-gray-100 p-2 pl-0"
               >
-                <img src="https://via.placeholder.com/100" />
-                <div className="flex-grow ml-8">
-                  <div className="flex justify-between mb-4">
-                    <div className="flex">
-                      <div className="text-sm text-white py-1 px-3 mr-2 bg-lightBlue-300">
-                        Tag1
-                      </div>
-                      <div className="text-sm text-white py-1 px-3 mr-2 bg-green-300">
-                        Tagtag2
-                      </div>
-                    </div>
-                    <div className="text-gray-300">
-                      {edge.node.publishedDate}
-                    </div>
-                  </div>
-                  <h2 className="text-2xl font-semibold">{edge.node.title}</h2>
-                </div>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </Layout>
+                <Link
+                  to={`/blog/${edge.node.slug}/`}
+                  className="flex items-center text-md font-light"
+                >
+                  <Img
+                    fluid={edge.node.mainImage.fluid}
+                    alt={edge.node.title}
+                    className="flex-shrink-0 w-20 h-12"
+                  />
+                  <h3 className="ml-4 font-normal">{edge.node.title}</h3>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+
+      <section className=" bg-green-100 py-20 px-8">
+        <h2 className="text-lg font-semibold text-green-400 tracking-wider text-center mb-12">
+          WHERE TO START
+        </h2>
+        <ul className="max-w-screen-lg mx-auto grid grid-cols-3 gap-8">
+          {categories.edges.map((edge: categoryProps) => {
+            return (
+              <BackgroundImg
+                Tag="li"
+                fluid={edge.node.image.fluid}
+                key={edge.node.id}
+                className="h-40 relative"
+              >
+                <div className="bg-blue-600 opacity-50 absolute inset-0"></div>
+                <Link
+                  to={`/blog/${edge.node.slug}/`}
+                  className="text-3xl text-white font-normal tracking-wide absolute inset-0 flex items-center justify-center text-center p-4"
+                >
+                  {edge.node.name}
+                </Link>
+              </BackgroundImg>
+            );
+          })}
+        </ul>
+      </section>
+    </div>
   );
 };
 
