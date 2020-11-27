f552276207f9cb72fd31ca9d8b8cdc59c2c8caa9
nozomail <nozomi.mail@icloud.com>
2020-11-27 23:23:28 +1300

    Add Footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="text-center tracking-wider py-2">
+      <small>&copy;2020 Biofab.blog</small>
+    </footer>
+  );
+};
+
+export default Footer;
