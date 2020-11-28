7084771c7c95f49e0cc83b15929aa8e37def18a5
nozomail <nozomi.mail@icloud.com>
2020-11-28 23:54:35 +1300

    Add styles to article

--- src/templates/article.tsx
+++ src/templates/article.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { graphql, Link } from 'gatsby';
+import Img, { FluidObject } from 'gatsby-image';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
-import Layout from '../components/layout';
+import Header from '../components/header';
+import Footer from '../components/footer';
+
+const colors = [
+  'pink-300',
+  'blue-300',
+  'lightBlue-300',
+  'green-300',
+  'gray-300',
+];
 
 export const query = graphql`
   query($slug: String!) {
@@ -12,7 +22,12 @@ export const query = graphql`
         name
         order
       }
-      publishedDate(formatString: "Do MMMM, YYYY")
+      publishedDate(formatString: "DD MMMM, YYYY")
+      mainImage {
+        fluid(maxWidth: 1280) {
+          ...GatsbyContentfulFluid
+        }
+      }
       body {
         raw
       }
@@ -26,9 +41,13 @@ type dataProps = {
       title: string;
       category: {
         name: string;
+        slug: string;
         order: number;
       };
       publishedDate: string;
+      mainImage: {
+        fluid: FluidObject;
+      };
       body: {
         raw: string;
       };
@@ -37,23 +56,50 @@ type dataProps = {
 };
 
 const BlogPost: React.FC<dataProps> = ({ data }) => {
+  const {
+    title,
+    category,
+    publishedDate,
+    mainImage,
+    body,
+  } = data.contentfulBlogPost;
+
+  console.log(body.raw);
   return (
-    <Layout
-      title={data.contentfulBlogPost.title}
-      color={data.contentfulBlogPost.category.order % 5}
-    >
-      <Link to="/articles">Article List</Link>
-      <div className="content">
-        <h1>{data.contentfulBlogPost.title}</h1>
-        <div>{data.contentfulBlogPost.category.name}</div>
-        <div className="meta">
-          Posted on {data.contentfulBlogPost.publishedDate}
+    <div>
+      <div className="pt-2 pb-8 px-8">
+        <Header />
+        <div className="text-center">
+          <h1
+            className={`max-w-screen-lg mx-auto text-4xl leading-normal font-serif mt-6 mb-4`}
+          >
+            {title}
+          </h1>
+          <Link
+            to={`/${category.slug}`}
+            className={`text-${colors[category.order % 5]} border-b border-${
+              colors[category.order % 5]
+            } font-normal pb-1`}
+          >
+            {category.name}
+          </Link>
         </div>
-        {documentToReactComponents(
-          JSON.parse(data.contentfulBlogPost.body.raw)
-        )}
       </div>
-    </Layout>
+      <div>
+        <Img fluid={mainImage.fluid} alt={title} className="h-80" />
+
+        <div className="py-8 px-8">
+          <div className="text-sm mb-4 text-right">{publishedDate}</div>
+          {documentToReactComponents(JSON.parse(body.raw))}
+          <div className="mt-4">
+            <Link to="/articles" className="border-b pb-1">
+              See all articles
+            </Link>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
   );
 };
 
