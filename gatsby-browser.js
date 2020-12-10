06fe8e9f04984136291996964401c7b7ab6ce1af
nozomail <nozomi.mail@icloud.com>
2020-12-10 21:48:21 +0900

    Add loading screen

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,8 @@
 import './src/utilities/styles/index.css';
+
+export const onInitialClientRender = () => {
+  setTimeout(function () {
+    document.getElementById('___loader').style.opacity = '0';
+    document.getElementById('___loader').style.pointerEvents = 'auto';
+  }, 1000);
+};
